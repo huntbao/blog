8577e8f6fa20ae6d29a5d45790bf684da360efb3
HuntBao <huntbao@gmail.com>
2024-10-09 20:14:28 +0800

    feat: 修改文档

--- _includes/algorithm/2024-10-09-algorithm-hard-js-longest-valid-parentheses.js
+++ _includes/algorithm/2024-10-09-algorithm-hard-js-longest-valid-parentheses.js
@@ -1,13 +1,14 @@
 
 // 这是题目要求的输入格式
 let inputs = `
-)()(()))
+()(()
 `;
 
 inputs = inputs.trim();
 
 function longestValidParentheses(s) {
-  const stack = [-1]; // 初始化栈，放入一个初始值 -1
+  // 初始值 -1 确保了在计算第一个有效括号子串长度时，能够正确计算长度。例如，对于字符串 ()()，在遇到第一个 ) 时，栈中只有 -1，计算长度为 1 - (-1) = 2。
+  const stack = [-1];
   let maxLength = 0;
 
   for (let i = 0; i < s.length; i++) {
@@ -16,9 +17,13 @@ function longestValidParentheses(s) {
     } else {
       stack.pop(); // 右括号，弹出栈顶元素
       if (stack.length === 0) {
-        stack.push(i); // 如果栈为空，放入当前右括号的索引
+        // 如果栈为空，说明当前右括号没有匹配的左括号，将当前索引 i 压入栈中，作为新的基准。
+        stack.push(i);
       } else {
-        maxLength = Math.max(maxLength, i - stack[stack.length - 1]); // 计算有效括号长度
+        // 如果栈不为空，计算当前有效括号子串的长度，即 i - stack[stack.length - 1]，并更新 maxLength。
+        // maxLength = i - stack[stack.length - 1]; // 错误写法，不能通过用例 "()(()"。
+        maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
+        console.log('maxLength:', maxLength);
       }
     }
   }
--- _includes/algorithm/2024-10-09-algorithm-hard-js-triplets-with-bitwise-and-equal-to-zero.js
+++ _includes/algorithm/2024-10-09-algorithm-hard-js-triplets-with-bitwise-and-equal-to-zero.js
@@ -28,5 +28,31 @@ function countTriplets(nums) {
   return count;
 }
 
+
+function countTripletss(nums) {
+  const n = nums.length;
+  const countMap = new Map();
+
+  for(let i = 0; i < n; i++) {
+      for(let j = 0; j < n; j++) {
+          const andResult = nums[i] & nums[j];
+          countMap.set(andResult, (countMap.get(andResult) || 0) + 1);
+      }
+  }
+
+  let count = 0;
+
+  for(let k = 0; k < n; k++) {
+      for(const [andResult, freq] of countMap) {
+          if (andResult & nums[k] === 0) {
+              count += freq;
+          }
+      }
+  }
+
+  return count;
+}
+
 // 示例
 console.log(countTriplets(inputs.split(',').map(Number))); // 不使用 .map(Number) 的话测试用例会超时
+console.log(countTripletss(inputs.split(',').map(Number))); // 不使用 .map(Number) 的话测试用例会超时
